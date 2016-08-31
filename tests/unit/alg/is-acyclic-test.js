bb3c853acd139a95a2a2a8d5c5f24318c5e6636b
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-31 09:35:27 -0700

    Bring in tests and fix build

---
+++ tests/unit/alg/is-acyclic-test.js
@@ -0,0 +1,29 @@
+import chai from '../chai'
+const {expect} = chai
+import {alg, Graph} from 'graphlib'
+const {isAcyclic} = alg
+import {describe, it} from 'mocha'
+
+describe('alg.isAcyclic', function () {
+  it('returns true if the graph has no cycles', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'c'])
+    expect(isAcyclic(g)).to.be.true
+  })
+
+  it('returns false if the graph has at least one cycle', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'c', 'a'])
+    expect(isAcyclic(g)).to.be.false
+  })
+
+  it('returns false if the graph has a cycle of 1 node', function () {
+    var g = new Graph()
+    g.setPath(['a', 'a'])
+    expect(isAcyclic(g)).to.be.false
+  })
+
+  it('rethrows non-CycleException errors', function () {
+    expect(function () { isAcyclic(undefined) }).to.throw()
+  })
+})
---
+++ tests/unit/alg/tarjan-test.js
@@ -0,0 +1,46 @@
+import _ from 'lodash'
+import chai from '../chai'
+const {expect} = chai
+import {alg, Graph} from 'graphlib'
+const {tarjan} = alg
+import {describe, it} from 'mocha'
+
+describe('alg.tarjan', function () {
+  it('returns an empty array for an empty graph', function () {
+    expect(tarjan(new Graph())).to.eql([])
+  })
+
+  it('returns singletons for nodes not in a strongly connected component', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'c'])
+    g.setEdge('d', 'c')
+    expect(sort(tarjan(g))).to.eql([['a'], ['b'], ['c'], ['d']])
+  })
+
+  it('returns a single component for a cycle of 1 edge', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'a'])
+    expect(sort(tarjan(g))).to.eql([['a', 'b']])
+  })
+
+  it('returns a single component for a triangle', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'c', 'a'])
+    expect(sort(tarjan(g))).to.eql([['a', 'b', 'c']])
+  })
+
+  it('can find multiple components', function () {
+    var g = new Graph()
+    g.setPath(['a', 'b', 'a'])
+    g.setPath(['c', 'd', 'e', 'c'])
+    g.setNode('f')
+    expect(sort(tarjan(g))).to.eql([['a', 'b'], ['c', 'd', 'e'], ['f']])
+  })
+})
+
+// A helper that sorts components and their contents
+function sort (cmpts) {
+  return _.sortBy(_.map(cmpts, function (cmpt) {
+    return _.sortBy(cmpt)
+  }), function (cmpts) { return cmpts[0] })
+}
